81acfd8bd7ce455860188c108e1bf0ebfc1f7f3d
Wallace Oliveira <89879348+itsmewall@users.noreply.github.com>
2022-10-24 12:43:46 -0300

    Consumo de API em Albums e Users

--- src/Albums.js
+++ src/Albums.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Albums = () => {
+    const [loading, setLoading] = useState(false)
+    const [albums, setAlbums] = useState([])
+
+    useEffect(() => {
+        setLoading(true)
+
+        fetch('https://jsonplaceholder.typicode.com/albums')
+        .then(response => response.json())
+        .then(data => {
+            setAlbums(data)
+            setLoading(false)
+        })
+    }, [])
+
     return (
-        <h1>Álbuns</h1>
+        <>
+            <h1>Álbuns</h1>
+            {
+                loading === true ? 'Carregando...' : ''
+            }
+            {
+                albums.map(album => {
+                    return (
+                        <div>
+                            <span>{album.title} - id: {album.id}</span>
+                        </div>
+                    )
+                })
+            }
+        </>
     )
 }
 
